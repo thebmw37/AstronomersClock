cd562a2459a4a0195f231d6fb66cfca76f0bce91
thebmw37 <brycewoods37@gmail.com>
2022-02-24 20:03:38 -0700

    Got a ticking counter

--- app/src/main/java/com/brycewoods/framework/astronomersclock/MainActivity.kt
+++ app/src/main/java/com/brycewoods/framework/astronomersclock/MainActivity.kt
@@ -4,16 +4,33 @@
 
 package com.brycewoods.framework.astronomersclock
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.AttributeSet
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Set background color to black
-        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+        val clock0 = supportFragmentManager.findFragmentById(R.id.fragment0) as ClockFragment
+
+        val handler1 = Looper.myLooper()?.let { Handler(it) }
+
+        var runnable1: Runnable? = null
+
+        runnable1 = Runnable {
+            clock0.update()
+            runnable1?.let { handler1?.postDelayed(it, 200) }
+        }
+
+        handler1?.postDelayed(runnable1, 200)
     }
+
 }
